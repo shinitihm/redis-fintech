167b344435052b57d7c14d3ea8e16d295d734262
shinitihm <heitor.shiniti09@gmail.com>
2026-09-16 09:16:04 -0300

    feat: multiservice commit solving 1 question

--- src/multiservice_commit.py
+++ src/multiservice_commit.py
@@ -13,3 +13,27 @@ r = redis.Redis(
 chave_pix = "pix:cliente:3300:semana1"
 chave_push = "push:cliente:3300:semana1"
 
+
+# ==========================================
+# A) Registrar eventos Pix e Push
+# ==========================================
+
+# Cliente usou Pix:
+# Terça (Bit 2)
+# Sexta (Bit 5)
+
+r.setbit(chave_pix, 2, 1)
+r.setbit(chave_pix, 5, 1)
+
+
+# Cliente abriu Push:
+# Segunda (Bit 1)
+# Terça (Bit 2)
+# Quinta (Bit 4)
+
+r.setbit(chave_push, 1, 1)
+r.setbit(chave_push, 2, 1)
+r.setbit(chave_push, 4, 1)
+
+
+print("Eventos registrados com sucesso!")
