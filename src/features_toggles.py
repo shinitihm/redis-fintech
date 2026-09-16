4432d0c27ae65f4e231465b072f9f0cae2e1de07
shinitihm <heitor.shiniti09@gmail.com>
2026-09-16 09:11:25 -0300

    chore: solving feature toggle 1 question

--- src/features_toggles.py
+++ src/features_toggles.py
@@ -12,3 +12,27 @@ r = redis.Redis(
 alfa = "permissoes:pj:1001"
 beta = "permissoes:pj:1002"
 
+
+# =====================================
+# A) Ativar permissões da Alfa Tech
+# =====================================
+
+def ativar_modulos(chave, modulos):
+    for modulo in modulos:
+        r.setbit(chave, modulo, 1)
+
+
+# Alfa Tech:
+# Pix (0)
+# Boletos (1)
+# Antecipação (3)
+# Cartão Corporativo (5)
+
+ativar_modulos(
+    alfa,
+    [0, 1, 3, 5]
+)
+
+
+print("Permissões Alfa Tech ativadas")
+
